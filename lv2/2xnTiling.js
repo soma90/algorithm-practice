cd60e88ea84914e6af7e62ee262ee9904adb3676
soma <macaronieyes@gmail.com>
2023-08-08 02:11:35 +0900

    fix: 2xnTiling

--- lv2/2xnTiling.js
+++ lv2/2xnTiling.js
@@ -41,10 +41,10 @@ Imgur
 function solution(n) {
   let arr = [1 , 2];
   for(let i = 2; i < n; i++) {
-    arr[i] = (arr[i - 2] + arr[i - 1]);
+    arr[i] = (arr[i - 2] + arr[i - 1]) % 1000000007;
   }
   return arr[n - 1];
 }
 
-let result = solution(50);
+let result = solution(4);
 console.log(result);
