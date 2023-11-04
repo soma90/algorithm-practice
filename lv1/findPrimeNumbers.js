1d691a586d6c0acdd99e0ab8a45cf510df364c6a
soma <macaronieyes@gmail.com>
2023-11-05 00:12:04 +0900

    fix: findPrimeNumbers

--- lv1/findPrimeNumbers.js
+++ lv1/findPrimeNumbers.js
@@ -1,26 +1,44 @@
+/* 
+https://school.programmers.co.kr/learn/courses/30/lessons/12921
+
+---소수 찾기---
+
+--문제 설명
+1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
+
+소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
+(1은 소수가 아닙니다.)
+
+--제한 조건
+n은 2이상 1000000이하의 자연수입니다.
+
+--입출력 예
+n	result
+10	4
+5	3
+
+--입출력 예 설명
+-입출력 예 #1
+1부터 10 사이의 소수는 [2,3,5,7] 4개가 존재하므로 4를 반환
+
+-입출력 예 #2
+1부터 5 사이의 소수는 [2,3,5] 3개가 존재하므로 3를 반환
+*/
+
 function solution(n) {
   let isPrime = Array.from({ length: n + 1 }, () => true);
   isPrime[0] = false;
   isPrime[1] = false;
 
-  for (let i = 2; i <= n; i++) {
-    //현재 숫자가 소수가 아니면 다음 숫자 확인
+  for (let i = 2; i * i <= n; i++) {
     if (isPrime[i] === false) continue;
 
-    //소수인지 확인
-    for (let j = 2; j * j <= i; j++) {
-      if (i % j === 0) {
-        isPrime[i] = false;
-        //찾은 소수가 아닌수의 배수 지우기
-        for (let k = j + j; k <= n; k += j) {
-          isPrime[k] = false;
-        }
-        break;
-      }
+    for (let j = i * i; j <= n; j += i) {
+      isPrime[j] = false;
     }
   }
 
-  return isPrime.filter(el => el).length;
+  return isPrime.filter((el) => el).length;
 }
 
 let result = solution(5);
